0d0cbf3b8f43f8b3992304e51d6479716f49eec0
hundun <wwj.hddjj@gmail.com>
2022-01-18 01:15:37 +0800

    update

--- core/src/hundun/gdxgame/idlepizza/logic/GameDictionary.java
+++ core/src/hundun/gdxgame/idlepizza/logic/GameDictionary.java
@@ -11,8 +11,28 @@ public class GameDictionary implements IGameDictionary {
     
     public String constructionIdToShowName(String constructionId) {
         switch (constructionId) {
-        
-
+            case ConstructionId.DOUGH_PROVIDER:
+                return "DOUGH_PROVIDER";
+            case ConstructionId.CHESS_PROVIDER:
+                return "CHESS_PROVIDER";
+            case ConstructionId.HAM_PROVIDER:
+                return "HAM_PROVIDER";
+            case ConstructionId.SPICE_PROVIDER:
+                return "SPICE_PROVIDER";
+            case ConstructionId.TOMATO_PROVIDER:
+                return "TOMATO_PROVIDER";      
+                
+            case ConstructionId.CLASSICAL_PIZZA_MAKER:
+                return "CLASSICAL_PIZZA";
+            case ConstructionId.DOUBLE_PIZZA_MAKER:
+                return "DOUBLE_PIZZA";
+            case ConstructionId.SPICE_PIZZA_MAKER:
+                return "SPICE_PIZZA";
+            case ConstructionId.TOMATO_PIZZA_MAKER:
+                return "TOMATO_PIZZA";
+            case ConstructionId.SUPER_PIZZA_MAKER:
+                return "SUPER_PIZZA";    
+                
             case ConstructionId.WIN_THE_GAME:
                 return "win goal";
 
